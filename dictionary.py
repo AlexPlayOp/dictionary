973610a0fefece20f484a641f90a8de365f8ab1c
AlexPlayOp <107238904+AlexPlayOp@users.noreply.github.com>
2022-11-05 13:26:47 +0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+import random
+
+file_name = 'test.txt'
+
+def main():
+    i = str(input())
+    if i == "список":
+        word_list = read_file(file_name)
+        for w in word_list:
+            print(w.strip())
+    if i == "Добавить":
+        b = str(input())
+        based.append(b)
+    if i == "случайно":
+        word_list = read_file(file_name)
+        n = int(input('сколько слов? '))
+        while n != 0:
+            b = random.randint(0, len(word_list)-1)
+            print(word_list[b].strip())
+            n -= 1
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        return f.readlines()
+
+if __name__ == "__main__":
+    main()
+
